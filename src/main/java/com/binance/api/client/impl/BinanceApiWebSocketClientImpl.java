5d9bae6bd1f110d89e968ac5980a7165355df97f
lishiwei <lishiwei>
2021-05-12 13:37:50 +0800

    use partial book depth

--- src/main/java/com/binance/api/client/impl/BinanceApiWebSocketClientImpl.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiWebSocketClientImpl.java
@@ -30,7 +30,7 @@ public class BinanceApiWebSocketClientImpl implements BinanceApiWebSocketClient,
     public Closeable onDepthEvent(String symbols, BinanceApiCallback<DepthEvent> callback) {
         final String channel = Arrays.stream(symbols.split(","))
                 .map(String::trim)
-                .map(s -> String.format("%s@depth", s))
+                .map(s -> String.format("%s@depth5@1000ms", s))
                 .collect(Collectors.joining("/"));
         return createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, DepthEvent.class));
     }
